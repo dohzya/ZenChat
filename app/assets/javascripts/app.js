424bcc5685ce21f9898e49afbf5d6abbf44e9d01
Étienne Vallette d'Osia <dohzya@gmail.com>
2013-02-08 09:18:58 +0100

    Messages are now always rendered in JS

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -17,6 +17,23 @@ window.ZenChat = (function(){
     });
   };
 
+  ZC.displayMessage = function(msg) {
+    var date = new Date(msg.date);
+    var fragment = '<article class="message" data-id="' + msg.id + '">' +
+       '<header>' +
+         '<div class="nickname">'+msg.author.name+'</div>' +
+         '<div class="avatar"><img src="'+msg.author.picture+'" class="img-rounded" alt="'+msg.author.name+'" /></div>' +
+       '</header>' +
+       '<section>' +
+         '<p>'+msg.text+'</p>' +
+       '</section>' +
+       '<footer>' +
+         '<div class="time">'+date.getHours()+':'+date.getMinutes()+'</div>' +
+       '</footer>' +
+     '</article>';
+    $("#messages").append(fragment);
+  };
+
   ZC.send = function(msg) {
     ZC.socket.send(JSON.stringify(msg));
   }
@@ -25,21 +42,8 @@ window.ZenChat = (function(){
     ZC.roomName = roomName;
     ZC.socket = socket;
     ZC.socket.onmessage = function(event) {
-      var data = JSON.parse(event.data);
-      var date = new Date(data.date);
-      var fragment = '<article class="message" data-id="' + data.id + '">' +
-         '<header>' +
-           '<div class="nickname">'+data.author.name+'</div>' +
-           '<div class="avatar"><img src="'+data.author.picture+'" class="img-rounded" alt="'+data.author.name+'" /></div>' +
-         '</header>' +
-         '<section>' +
-           '<p>'+data.text+'</p>' +
-         '</section>' +
-         '<footer>' +
-           '<div class="time">'+date.getHours()+':'+date.getMinutes()+'</div>' +
-         '</footer>' +
-       '</article>';
-      $("#messages").append(fragment);
+      var msg = JSON.parse(event.data);
+      ZC.displayMessage(msg);
     };
     ZC.socket.onopen = function(event) {};
     ZC.socket.onclose = function(event) {};
