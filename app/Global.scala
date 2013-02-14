eb2417715045818a38a43471b4f7e3d93c2bb38c
Étienne Vallette d'Osia <dohzya@gmail.com>
2013-02-14 12:51:09 +0100

    add Global

---
+++ app/Global.scala
@@ -0,0 +1,11 @@
+import play.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import models._
+
+object Global extends GlobalSettings {
+
+  override def onStart(app: Application) {
+  }
+
+}
